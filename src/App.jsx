712a1e88cfeae00f6f8cb4d26d640961558d691b
Harshsanket <Harshsanket.dev@gmail.com>
2024-04-09 16:47:17 +0530

    v2.1 cleaned up some code improved some buttons

--- src/App.jsx
+++ src/App.jsx
@@ -14,36 +14,38 @@ const App = () => {
     setActiveIndex2(activeIndex2 === index ? null : index);
   };
 
+  const [activeIndex3, setActiveIndex3] = useState(null);
+
+  const toggleAccordion3 = (index) => {
+    setActiveIndex3(activeIndex3 === index ? null : index);
+  };
+
   const [length, setLength] = useState(6);
-  const [number, setNumber] = useState(false);
-  const [uppercase, setUppercase] = useState(false);
-  const [lowercase, setLowerCase] = useState(false);
+  const [number, setNumber] = useState(true);
+  const [uppercase, setUppercase] = useState(true);
+  const [lowercase, setLowerCase] = useState(true);
   const [character, setCharacter] = useState(false);
   const [password, setPassword] = useState("");
-  const [specialS, setSpecials] = useState(false)
+  const [specialS, setSpecials] = useState(false);
   const [copied, setCopied] = useState(false);
-
+  const [savedData, setSavedData] = useState([]);
   const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "";
-    // let lowercase = "abcdefghijklmnopqrstuvwxyz";
-    // let uppercase = lowercase.toUpperCase();
-    // let str = lowercase + uppercase;
     if (number) str += "1234567890";
-    if (character) str += "!@#$%^&*()[]{}|:;<>,.?/~+";
+    if (character) str += "!@#$%&*";
     if (lowercase) str += "abcdefghijklmnopqrstuvwxyz";
     if (uppercase) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (specialS) str += "$€£¥A$C$Fr¥₹₩krNZ$Mex$S$HK$krR$₽R₺"
-
+    if (specialS) str += "$€£¥₹₩₽₺";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+      let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
     setPassword(pass);
-  }, [length, number, character, password,lowercase, uppercase, specialS ]);
+  }, [length, number, character, password, lowercase, uppercase, specialS]);
 
   const copyPassword = useCallback(() => {
     passwordRef.current?.select();
@@ -53,9 +55,37 @@ const App = () => {
     setTimeout(() => setCopied(false), 1000);
   }, [password]);
 
+  const copySavedPassword = (index) => {
+    const passwordToCopy = savedData[index];
+    navigator.clipboard.writeText(passwordToCopy);
+  };
+
   useEffect(() => {
     passwordGenerator();
-  }, [length, number, character,lowercase, uppercase, specialS]);
+  }, [length, number, character, lowercase, uppercase, specialS]);
+
+  useEffect(() => {
+    const data = localStorage.getItem("myPassword");
+    if (data) {
+      setSavedData(JSON.parse(data));
+    }
+  }, [setSavedData]);
+
+  function savePass() {
+    if (!savedData.includes(password)) {
+      const updatedData = [...savedData, password];
+      localStorage.setItem("myPassword", JSON.stringify(updatedData));
+      setSavedData(updatedData);
+    } else {
+      alert("This password is already saved!");
+    }
+  }
+
+  const deleteSavedData = (index) => {
+    const updatedData = savedData.filter((_, i) => i !== index); 
+    localStorage.setItem("myPassword", JSON.stringify(updatedData)); 
+    setSavedData(updatedData); 
+  };
   return (
     <>
       <div
@@ -90,63 +120,20 @@ const App = () => {
           >
             Generate
           </button>
-          <button className="text-white sm:text-lg text-sm bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full  px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+          <button
+            onClick={savePass}
+            className="text-white sm:text-lg text-sm bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full  px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          >
             Save
           </button>
         </div>
 
-        <div>
-          {/* <div className="w-full mt-4 max-w-md mx-auto rounded-xl px-4 py-0.5  bg-neutral-800 text-white">
-        <div className=" text-sm gap-x-2 mt-3 mb-2">
-          <div className="flex items-center gap-x-1 text-black">
-            <select
-              value={length}
-              onChange={(e) => {
-                setLength(parseInt(e.target.value));
-              }}
-              className="cursor-pointer"
-            >
-              {[...Array(9).keys()].map((num) => (
-                <option key={num + 4} value={num + 4}>
-                  {num + 4}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="length" className="text-white">Length: {length}</label>
-          </div>
-         <br />
-          <div className=" text-sm gap-x-2">
-            
-            <input
-              type="checkbox"
-              defaultChecked={number}
-              id="numberInput"
-              onChange={() => {
-                setNumber((prev) => !prev);
-              }}
-            />
-            <label htmlFor="numberInput">Numbers</label>
-          </div>
-          <br />
-          <div className=" text-sm gap-x-2">
-            <input
-              type="checkbox"
-              defaultChecked={character}
-              id="characterInput"
-              onChange={() => {
-                setCharacter((prev) => !prev);
-              }}
-            />
-            <label htmlFor="characterInput">Character</label>
-          </div>
-        </div>
-      </div> */}
-        </div>
+        <div></div>
       </div>
 
       <div className="mt-4 pl-4 pr-4 sm:flex sm:justify-center ">
         <div className="sm:w-2/5">
-          <div >
+          <div>
             <button
               className="flex justify-between items-center w-full p-5 font-medium  border-gray-200 rounded-xl bg-neutral-800 text-white"
               onClick={() => toggleAccordion(1)}
@@ -218,7 +205,7 @@ const App = () => {
                     <span className="p-2">Characters to include</span>
                     <svg
                       className={`w-4 h-4 transition-transform ${
-                        activeIndex === 2 ? "transform rotate-180" : ""
+                        activeIndex2 === 2 ? "transform rotate-180" : ""
                       }`}
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -236,15 +223,15 @@ const App = () => {
                     <div className=" text-white rounded-xl mt-2 border-neutral-400 bg-neutral-800">
                       <div className="p-2  border rounded-xl">
                         <div className="mt-2 flex justify-between items-center w-full font-medium   border-gray-200 rounded-xl bg-neutral-800 text-white">
-                          <label htmlFor="characterInput">
+                          <label htmlFor="uppercase">
                             Uppercase Alphabets
                             <br />
-                            <span className="text-gray-400">ABCDEFGHIJKLMNOPQRSTUVWXYZ</span>
+                            <span className="text-gray-400">A to Z</span>
                           </label>
                           <input
                             type="checkbox"
                             defaultChecked={uppercase}
-                            id="characterInput"
+                            id="uppercase"
                             onChange={() => {
                               setUppercase((prev) => !prev);
                             }}
@@ -252,15 +239,15 @@ const App = () => {
                           />
                         </div>
                         <div className="mt-2 flex justify-between items-center w-full font-medium   border-gray-200 rounded-xl bg-neutral-800 text-white">
-                          <label htmlFor="characterInput">
+                          <label htmlFor="lowercase">
                             Lowercase Alphabets
                             <br />
-                            <span className="text-gray-400">abcdefghijklmnopqrstuvwxyz</span>
+                            <span className="text-gray-400">a to z</span>
                           </label>
                           <input
                             type="checkbox"
                             defaultChecked={lowercase}
-                            id="characterInput"
+                            id="lowercase"
                             onChange={() => {
                               setLowerCase((prev) => !prev);
                             }}
@@ -271,7 +258,7 @@ const App = () => {
                           <label htmlFor="characterInput">
                             Symbols
                             <br />
-                            <span className="text-gray-400">@#$%^&*()_+</span>
+                            <span className="text-gray-400">!@#$%&*</span>
                           </label>
                           <input
                             type="checkbox"
@@ -284,15 +271,15 @@ const App = () => {
                           />
                         </div>
                         <div className="mt-2 flex justify-between items-center w-full font-medium   border-gray-200 rounded-xl bg-neutral-800 text-white">
-                          <label htmlFor="characterInput">
+                          <label htmlFor="specialS">
                             Special Symbols
                             <br />
-                            <span className="text-gray-400">$€£¥A$C$Fr¥₹₩krNZ$Mex$S$HK$krR$₽R₺</span>
+                            <span className="text-gray-400">$€£¥₹₩₽₺</span>
                           </label>
                           <input
                             type="checkbox"
                             defaultChecked={specialS}
-                            id="characterInput"
+                            id="specialS"
                             onChange={() => {
                               setSpecials((prev) => !prev);
                             }}
@@ -306,35 +293,95 @@ const App = () => {
               </div>
             )}
           </div>
-          {/* <div className="mb-4">
-        <button
-          className="flex justify-between items-center w-full p-5 font-medium text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800"
-          onClick={() => toggleAccordion(2)}
-        >
-          <span>How do I install Tailwind CSS?</span>
-          <svg
-            className={`w-4 h-4 transition-transform ${
-              activeIndex === 2 ? 'transform rotate-180' : ''
-            }`}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-        {activeIndex === 2 && (
-          <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-            <p className="text-gray-500 dark:text-gray-400">
-              You can install Tailwind CSS using npm or yarn. First, install it as a dev dependency: npm install tailwindcss --save-dev or yarn add tailwindcss --dev. Then, create a tailwind.config.js file using npx tailwindcss init or yarn run tailwindcss init.
-            </p>
+        </div>
+      </div>
+
+      <div className="mt-4 pl-4 pr-4 sm:flex sm:justify-center ">
+        <div className="sm:w-2/5">
+          <div>
+            <button
+              className="flex justify-between items-center w-full p-5 font-medium  border-gray-200 rounded-xl bg-neutral-800 text-white"
+              onClick={() => toggleAccordion3(1)}
+            >
+              <span>Show Saved Password</span>
+              <svg
+                className={`w-4 h-4 transition-transform ${
+                  activeIndex3 === 1 ? "transform rotate-180" : ""
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            {activeIndex3 === 1 && (
+              <div className=" text-white rounded-xl mt-2 ">
+                {/* first option */}
+                {savedData.length > 0 ? (
+                  <div className=" text-white rounded-xl mt-2 border-neutral-400 bg-neutral-800">
+                    <div className="p-2  border rounded-xl">
+                      <div>
+                        <ul>
+                          {savedData.map((password, index) => (
+                            <li
+                              key={index}
+                              className="border rounded-lg mt-2 p-2 flex justify-between items-center"
+                            >
+                              <span>
+                                {index + 1} - {password}
+                              </span>
+                              <div className="flex gap-2">
+                                <button className="rounded-full w-8 h-8 bg-green-500 text-white flex items-center">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 448 512"
+                                    className="h-4 w-4 ml-2 cursor-pointer"
+                                    onClick={() => copySavedPassword(index)}
+                                  >
+                                    <path
+                                      fill="white"
+                                      d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"
+                                    />
+                                  </svg>
+                                </button>
+                                <button
+                                  className="rounded-full w-8 h-8 bg-red-500 text-white flex items-center justify-center"
+                                  onClick={() => deleteSavedData(index)}
+                                >
+                                  <svg
+                                    className="w-4 h-4 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M6 18L18 6M6 6l12 12"
+                                    />
+                                  </svg>
+                                </button>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="p-4 text-neutral-400 rounded-xl mt-2 border-neutral-400 bg-neutral-800">
+                    <span>No saved passwords to display.</span>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
-        )}
-      </div> */}
         </div>
       </div>
     </>
